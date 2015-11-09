e9d2837eebd2f7434ae2f7a3b6ce29bd08f2c2f1
Bianca Padilla <biancalpadilla@gmail.com>
2015-11-09 17:26:34 -0500

    Added real- images to dragndrop, created video page and css

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -8,16 +8,25 @@ var attempts = 0;
 var correct = 0;
 
 var captions = [];
-captions[0] = "USB Test 1";
-captions[1] = "USB Test 2";
-captions[2] = "Ethernet Test";
-captions[3] = "USB and Ethernet Controller";
-captions[4] = "Video and Audio Controller";
-captions[5] = "Camera CSI";
-captions[6] = "CPU / GPU";
-captions[7] = "HDMI";
-captions[8] = "Micro USB";
-captions[9] = "Display DSI";
+captions[0] =
+  "USB is short for Universal Serial Bus. A single USB port can be used to connect your Raspberry Pi to other devices, such as mice, modems, and keyboards.";
+captions[1] =
+  "USB is short for Universal Serial Bus. A single USB port can be used to connect your Raspberry Pi to other devices, such as mice, modems, and keyboards.";
+captions[2] =
+  "An Ethernet port allows your to connect your Raspberry Pi to the Internet using a WiFi router.";
+captions[3] =
+  "The USB and Ethernet controller controls all inputs and outputs related to USB and Internet interaction.";
+captions[4] =
+  "Video and Audio Controller controls all inputs and outputs related to video and audio interaction.";
+captions[5] =
+  "Camera Serial Interface, or CSI for short, is a ribbon connector that allows your Raspberry Pi to <use></use> a camera.";
+captions[6] =
+  "CPU means Central Processing Unit. The CPU is the brains of the computer where most calculations take place. A GPU means Graphics Processing Unit, and is used to create and show images on your screen.";
+captions[7] =
+  "HDMI allows you to connect your Raspberry Pi to a TV.";
+captions[8] = "The Micro USB gives your Raspberry Pi power.";
+captions[9] =
+  "Display Serial Interface, or DSI for short, is a ribbon connector that allows your Raspberry Pi to use a display, like a computer monitor.";
 
 function dragndropInit() {
   $("#detail")
@@ -82,7 +91,7 @@ function showInfo(which) {
   $("#detail #desc").text(captions[which]);
 
   var img = new Image();
-  img.src = "imgages/real-" + allParts[which] + ".jpg";
+  img.src = "images/real-" + allParts[which] + ".jpg";
   $("#detail #photoCon").empty();
   $("#detail #photoCon").append(img);
 }
--- js/jquery.quiz.js
+++ js/jquery.quiz.js
@@ -1,9 +1,9 @@
 ﻿/* Built using a tutorial from: Tutsplus
 https://code.tutsplus.com/tutorials/build-a-spiffy-quiz-engine--net-20185*/
 
-( function ( $ ) {
+(function ($) {
 
-  $.fn.jquizzy = function ( settings ) {
+  $.fn.jquizzy = function (settings) {
 
     var defaults = {
       questions: null,
@@ -32,16 +32,16 @@ https://code.tutsplus.com/tutorials/build-a-spiffy-quiz-engine--net-20185*/
 
     };
 
-    var config = $.extend( defaults, settings );
-    if ( config.questions === null ) {
-      $( this ).html(
+    var config = $.extend(defaults, settings);
+    if(config.questions === null) {
+      $(this).html(
         '<div class="intro-container slide-container"><h2 class="qTitle">Failed to parse questions.</h2></div>'
       );
       return;
     }
 
-    var superContainer = $( this ),
-      answers = [ ],
+    var superContainer = $(this),
+      answers = [],
       introFob =
       ' <div class="intro-container slide-container"><div class="question-number">' +
       config.startText + '</div><a class="nav-start" href="#"><img src="' +
@@ -52,29 +52,29 @@ https://code.tutsplus.com/tutorials/build-a-spiffy-quiz-engine--net-20185*/
       '</div><div class="result-keeper"></div></div><div class="notice">Please choose an answer.</div><div class="progress-keeper" ><div class="progress"></div></div>',
       contentFob = '',
       questionsIteratorIndex, answersIteratorIndex;
-    superContainer.addClass( 'main-quiz-holder' );
+    superContainer.addClass('main-quiz-holder');
 
-    for ( questionsIteratorIndex = 0; questionsIteratorIndex < config.questions
-      .length; questionsIteratorIndex++ ) {
+    for(questionsIteratorIndex = 0; questionsIteratorIndex < config.questions
+      .length; questionsIteratorIndex++) {
       contentFob +=
         '<div class="slide-container"><div class="question-number">' + (
-          questionsIteratorIndex + 1 ) + '/' + config.questions.length +
+          questionsIteratorIndex + 1) + '/' + config.questions.length +
         '</div><div class="question">' + config.questions[
-          questionsIteratorIndex ].question + '</div><ul class="answers">';
-      for ( answersIteratorIndex = 0; answersIteratorIndex < config.questions[
-          questionsIteratorIndex ].answers.length; answersIteratorIndex++ ) {
-        contentFob += '<li>' + config.questions[ questionsIteratorIndex ].answers[
-          answersIteratorIndex ] + '</li>';
+          questionsIteratorIndex].question + '</div><ul class="answers">';
+      for(answersIteratorIndex = 0; answersIteratorIndex < config.questions[
+          questionsIteratorIndex].answers.length; answersIteratorIndex++) {
+        contentFob += '<li>' + config.questions[questionsIteratorIndex].answers[
+          answersIteratorIndex] + '</li>';
       }
 
       contentFob += '</ul><div class="nav-container">';
 
-      if ( questionsIteratorIndex !== 0 ) {
+      if(questionsIteratorIndex !== 0) {
         contentFob +=
           '<div class="prev"><a class="nav-previous btn btn-info btn-raised" href="#">Prev</a></div>';
       }
 
-      if ( questionsIteratorIndex < config.questions.length - 1 ) {
+      if(questionsIteratorIndex < config.questions.length - 1) {
         contentFob +=
           '<div class="next"><a class="nav-next btn btn-success btn-raised" href="#">Next</a></div>';
       } else {
@@ -83,196 +83,198 @@ https://code.tutsplus.com/tutorials/build-a-spiffy-quiz-engine--net-20185*/
       }
 
       contentFob += '</div></div>';
-      answers.push( config.questions[ questionsIteratorIndex ].correctAnswer );
+      answers.push(config.questions[questionsIteratorIndex].correctAnswer);
     }
 
-    superContainer.html( introFob + contentFob + exitFob );
+    superContainer.html(introFob + contentFob + exitFob);
 
-    var progress = superContainer.find( '.progress' ),
-      progressKeeper = superContainer.find( '.progress-keeper' ),
-      notice = superContainer.find( '.notice' ),
-      progressWidth = progressKeeper.width( ),
-      userAnswers = [ ],
+    var progress = superContainer.find('.progress'),
+      progressKeeper = superContainer.find('.progress-keeper'),
+      notice = superContainer.find('.notice'),
+      progressWidth = progressKeeper.width(),
+      userAnswers = [],
       questionLength = config.questions.length,
-      slidesList = superContainer.find( '.slide-container' );
+      slidesList = superContainer.find('.slide-container');
 
-    function checkAnswers( ) {
-      var resultArr = [ ],
+    function checkAnswers() {
+      var resultArr = [],
         flag = false;
-      for ( i = 0; i < answers.length; i++ ) {
+      for(i = 0; i < answers.length; i++) {
 
-        if ( answers[ i ] == userAnswers[ i ] ) {
+        if(answers[i] == userAnswers[i]) {
           flag = true;
         } else {
           flag = false;
         }
-        resultArr.push( flag );
+        resultArr.push(flag);
       }
       return resultArr;
     }
 
-    function roundReloaded( num, dec ) {
-      var result = Math.round( num * Math.pow( 10, dec ) ) / Math.pow( 10,
-        dec );
+    function roundReloaded(num, dec) {
+      var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10,
+        dec);
       return result;
     }
 
-    function judgeSkills( score ) {
+    function judgeSkills(score) {
       var returnString;
-      if ( score === 100 )
+      if(score === 100)
         return config.resultComments.perfect;
-      else if ( score > 90 )
+      else if(score > 90)
         return config.resultComments.excellent;
-      else if ( score > 70 )
+      else if(score > 70)
         return config.resultComments.good;
-      else if ( score > 50 )
+      else if(score > 50)
         return config.resultComments.average;
-      else if ( score > 35 )
+      else if(score > 35)
         return config.resultComments.bad;
-      else if ( score > 20 )
+      else if(score > 20)
         return config.resultComments.poor;
       else
         return config.resultComments.worst;
     }
 
-    progressKeeper.hide( );
-    notice.hide( );
-    slidesList.hide( ).first( ).fadeIn( 500 );
+    progressKeeper.hide();
+    notice.hide();
+    slidesList.hide().first().fadeIn(500);
 
-    superContainer.find( 'li' ).click( function ( ) {
-      var thisLi = $( this );
+    superContainer.find('li').click(function () {
+      var thisLi = $(this);
 
-      if ( thisLi.hasClass( 'selected' ) ) {
-        thisLi.removeClass( 'selected' );
+      if(thisLi.hasClass('selected')) {
+        thisLi.removeClass('selected');
       } else {
-        thisLi.parents( '.answers' ).children( 'li' ).removeClass(
-          'selected' );
-        thisLi.addClass( 'selected' );
+        thisLi.parents('.answers').children('li').removeClass(
+          'selected');
+        thisLi.addClass('selected');
       }
-    } );
+    });
 
-    superContainer.find( '.nav-start' ).click( function ( ) {
+    superContainer.find('.nav-start').click(function () {
 
-      $( this ).parents( '.slide-container' ).fadeOut( 500, function ( ) {
-        $( this ).next( ).fadeIn( 500 );
-        progressKeeper.fadeIn( 500 );
-      } );
+      $(this).parents('.slide-container').fadeOut(500, function () {
+        $(this).next().fadeIn(500);
+        progressKeeper.fadeIn(500);
+      });
       return false;
 
-    } );
+    });
 
-    superContainer.find( '.next' ).click( function ( ) {
+    superContainer.find('.next').click(function () {
 
-      if ( $( this ).parents( '.slide-container' ).find( 'li.selected' )
-        .length === 0 ) {
-        notice.fadeIn( 300 );
+      if($(this).parents('.slide-container').find('li.selected')
+        .length === 0) {
+        notice.fadeIn(300);
         return false;
       }
 
-      notice.hide( );
-      $( this ).parents( '.slide-container' ).fadeOut( 500, function ( ) {
-        $( this ).next( ).fadeIn( 500 );
-      } );
-      progress.animate( {
-        width: progress.width( ) + Math.round( progressWidth /
-          questionLength )
-      }, 500 );
+      notice.hide();
+      $(this).parents('.slide-container').fadeOut(500, function () {
+        $(this).next().fadeIn(500);
+      });
+      progress.animate({
+        width: progress.width() + Math.round(progressWidth /
+          questionLength)
+      }, 500);
       return false;
-    } );
+    });
 
-    superContainer.find( '.prev' ).click( function ( ) {
-      notice.hide( );
-      $( this ).parents( '.slide-container' ).fadeOut( 500, function ( ) {
-        $( this ).prev( ).fadeIn( 500 );
-      } );
+    superContainer.find('.prev').click(function () {
+      notice.hide();
+      $(this).parents('.slide-container').fadeOut(500, function () {
+        $(this).prev().fadeIn(500);
+      });
 
-      progress.animate( {
-        width: progress.width( ) - Math.round( progressWidth /
-          questionLength )
-      }, 500 );
+      progress.animate({
+        width: progress.width() - Math.round(progressWidth /
+          questionLength)
+      }, 500);
       return false;
-    } );
+    });
 
-    superContainer.find( '.final' ).click( function ( ) {
-      if ( $( this ).parents( '.slide-container' ).find( 'li.selected' )
-        .length === 0 ) {
-        notice.fadeIn( 300 );
+    superContainer.find('.final').click(function () {
+      if($(this).parents('.slide-container').find('li.selected')
+        .length === 0) {
+        notice.fadeIn(300);
         return false;
       }
 
-      superContainer.find( 'li.selected' ).each( function ( index ) {
-        userAnswers.push( $( this ).parents( '.answers' ).children(
-          'li' ).index( $( this ).parents( '.answers' ).find(
-          'li.selected' ) ) + 1 );
-      } );
+      superContainer.find('li.selected').each(function (index) {
+        userAnswers.push($(this).parents('.answers').children(
+          'li').index($(this).parents('.answers').find(
+          'li.selected')) + 1);
+      });
 
-      if ( config.sendResultsURL !== null ) {
-        var collate = [ ];
-        for ( r = 0; r < userAnswers.length; r++ ) {
-          collate.push( '{"questionNumber":"' + parseInt( r + 1, 10 ) +
-            '", "userAnswer":"' + userAnswers[ r ] + '"}' );
+      if(config.sendResultsURL !== null) {
+        var collate = [];
+        for(r = 0; r < userAnswers.length; r++) {
+          collate.push('{"questionNumber":"' + parseInt(r + 1, 10) +
+            '", "userAnswer":"' + userAnswers[r] + '"}');
         }
-        $.ajax( {
+        $.ajax({
           type: 'POST',
           url: config.sendResultsURL,
-          data: '{"answers": [' + collate.join( "," ) + ']}',
-          complete: function ( ) {
-            console.log( "OH HAI" );
+          data: '{"answers": [' + collate.join(",") + ']}',
+          complete: function () {
+            console.log("OH HAI");
           }
-        } );
+        });
       }
 
-      progressKeeper.hide( );
-      var results = checkAnswers( ),
+      progressKeeper.hide();
+      var results = checkAnswers(),
         resultSet = '',
         trueCount = 0,
         shareButton = '',
         score,
         url;
 
-      if ( config.shortURL === null ) {
+      if(config.shortURL === null) {
         config.shortURL = window.location
       };
 
-      for ( var i = 0, toLoopTill = results.length; i < toLoopTill; i++ ) {
-        if ( results[ i ] === true ) {
+      for(var i = 0, toLoopTill = results.length; i < toLoopTill; i++) {
+        if(results[i] === true) {
           trueCount++;
           isCorrect = true;
         }
-        resultSet += '<div class="result-row"> Question #' + ( i + 1 ) +
-          ( results[ i ] === true ?
+        resultSet += '<div class="result-row"> Question #' + (i + 1) +
+          (results[i] === true ?
             "<div class='correct btn btn-success btn-raised'><span>Correct</span></div>" :
-            "<div class='wrong btn btn-success btn-danger'><span>Incorrect</span></div>" );
+            "<div class='wrong btn btn-success btn-danger'><span>Incorrect</span></div>"
+          );
         resultSet += '<div class="resultsview-qhover">' + config.questions[
-          i ].question;
+          i].question;
         resultSet += "<ul>";
-        for ( answersIteratorIndex = 0; answersIteratorIndex < config.questions[
-            i ].answers.length; answersIteratorIndex++ ) {
+        for(answersIteratorIndex = 0; answersIteratorIndex < config.questions[
+            i].answers.length; answersIteratorIndex++) {
           var classestoAdd = '';
-          if ( config.questions[ i ].correctAnswer ==
-            answersIteratorIndex + 1 ) {
+          if(config.questions[i].correctAnswer ==
+            answersIteratorIndex + 1) {
             classestoAdd += 'right';
           }
-          if ( userAnswers[ i ] == answersIteratorIndex + 1 ) {
+          if(userAnswers[i] == answersIteratorIndex + 1) {
             classestoAdd += ' selected';
           }
           resultSet += '<li class="' + classestoAdd + '">' + config.questions[
-            i ].answers[ answersIteratorIndex ] + '</li>';
+            i].answers[answersIteratorIndex] + '</li>';
         }
         resultSet += '</ul></div></div>';
 
       }
-      score = roundReloaded( trueCount / questionLength * 100, 2 );
+      score = roundReloaded(trueCount / questionLength * 100, 2);
 
-      if ( config.addTwitter !== false ) {
-        shareButton += '<a style="margin-left: 37px;margin-right: 35px !important;" href="https://twitter.com/share?url=' +
-          encodeURIComponent( config.socialStatus.replace( "{score}",
-            score ) ) + ' @ ' + config.shortURL +
+      if(config.addTwitter !== false) {
+        shareButton +=
+          '<a style="margin-left: 37px;margin-right: 35px !important;" href="https://twitter.com/share?url=' +
+          encodeURIComponent(config.socialStatus.replace("{score}",
+            score)) + ' @ ' + config.shortURL +
           '" class="share-button-twitter" target="_blank" ><img src="' +
           config.twitterImage + '"  width="100" /></a>';
       }
 
-      if ( config.addFacebook !== false ) {
+      if(config.addFacebook !== false) {
         shareButton +=
           '<a style="margin-right: 35px !important;" href="https://www.facebook.com/sharer/sharer.php?u=' +
           config.shortURL +
@@ -280,29 +282,33 @@ https://code.tutsplus.com/tutorials/build-a-spiffy-quiz-engine--net-20185*/
           config.facebookImage + '"  width="100" /></a>';
       }
 
-      if ( config.addGooglePlus !== false ) {
-        shareButton += '<a style="margin-right: 12px !important;" href="https://plus.google.com/share?url=' +
+      if(config.addGooglePlus !== false) {
+        shareButton +=
+          '<a style="margin-right: 12px !important;" href="https://plus.google.com/share?url=' +
           config.shortURL +
           '" class="share-button-gp" target="_blank" ><img src="' +
           config.gPlusImage + '"  width="100" /></a>';
       }
 
-      resultSet = '<h2 class="qTitle">' + judgeSkills( score ) +
+      resultSet = '<h2 class="qTitle">' + judgeSkills(score) +
         ' You scored ' + score + '%</h2>' + shareButton +
         '<div class="jquizzy-clear"></div>' + resultSet +
-        '<div class="jquizzy-clear"></div>';
-      superContainer.find( '.result-keeper' ).html( resultSet ).show(
-        500 );
-      superContainer.find( '.resultsview-qhover' ).hide( );
-      superContainer.find( '.result-row' ).hover( function ( ) {
-        $( this ).find( '.resultsview-qhover' ).show( );
-      }, function ( ) {
-        $( this ).find( '.resultsview-qhover' ).hide( );
-      } );
-      $( this ).parents( '.slide-container' ).fadeOut( 500, function ( ) {
-        $( this ).next( ).fadeIn( 500 );
-      } );
+        '<div class="jquizzy-clear"></div>' +
+        '<div class="jquizzy-clear"></div>' +
+        '<a href="javascript:void(0)" class="btn btn-info btn-raised">Back</a>' +
+        '<a href="javascript:void(0)" class="btn btn-success nextbtn btn-raised">Next</a>';
+      superContainer.find('.result-keeper').html(resultSet).show(
+        500);
+      superContainer.find('.resultsview-qhover').hide();
+      superContainer.find('.result-row').hover(function () {
+        $(this).find('.resultsview-qhover').show();
+      }, function () {
+        $(this).find('.resultsview-qhover').hide();
+      });
+      $(this).parents('.slide-container').fadeOut(500, function () {
+        $(this).next().fadeIn(500);
+      });
       return false;
-    } );
+    });
   };
-} )( jQuery );
+})(jQuery);
