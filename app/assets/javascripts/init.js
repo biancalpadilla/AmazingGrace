3faa9869ab7e811bc1135bf4ef5ceb8e0ee901c6
Bianca Padilla <biancalpadilla@gmail.com>
2015-11-11 22:43:47 -0500

    Added init.js file to assets/javascript for quiz.htm

---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,27 @@
+﻿var init = {
+  'questions': [ {
+    'question': 'Question 1',
+    'answers': [ 'JavaScript library', 'Ruby Gem', 'PHP Framework',
+      'None of the above'
+    ],
+    'correctAnswer': 1
+  }, {
+    'question': 'Question 2',
+    'answers': [ 'P', 'W', 'Y', 'v' ],
+    'correctAnswer': 2
+  }, {
+    'question': 'Question 3',
+    'answers': [ 'Eh', '/Sigh', 'What?', 'Whatever' ],
+    'correctAnswer': 4
+  } ],
+  'resultComments': {
+    perfect: 'Perfect! Keep up the hard work!',
+    excellent: 'Outstanding, keep working hard!',
+    good: 'Outstanding, keep working hard!',
+    average: 'Good work. Keep working at it!',
+    bad: 'You can do better! Try again.',
+    poor: 'You can do better! Try again.',
+    worst: 'Oh no! Keep working hard and try again.'
+  }
+
+};
