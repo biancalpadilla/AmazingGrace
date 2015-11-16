747c7b2b4cf0b89a7c432d062df276d77d910084
Bianca Padilla <biancalpadilla@gmail.com>
2015-11-15 20:18:32 -0500

    Fixed cloud_tag bug in application_helper and question model, switched db from sqlite3 to psql

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,11 @@ module ApplicationHelper
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
 
-
-def tag_cloud(tags, classes)
-  max = tags.sort_by(&:count).last
-  tags.each do |tag|
-    index = tag.count.to_f / max.count * (classes.size - 1)
-    yield(tag, classes[index.round])
+  def tag_cloud(tags, classes)
+    max = tags.sort_by(&:count).last
+    tags.each do |tag|
+      index = tag.count.to_f / Integer(max.count) * (classes.size - 1)
+      yield(tag, classes[index.round])
+    end
   end
-end
 end
